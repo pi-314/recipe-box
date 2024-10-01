52c8cf5f4a2df76c3a6f54692b2c8acf360728d1
Paul Iov <pauliovster@gmail.com>
2024-10-01 19:08:50 +0200

    chore: add TailwindCSS support

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,13 @@
+// Type import not required in .ts
+// /** @type {import('tailwindcss').Config} */
+//
+export default {
+  content: [
+    './projects/**/*.{html,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
