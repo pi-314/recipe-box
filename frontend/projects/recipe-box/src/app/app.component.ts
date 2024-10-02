f0dc5a4c520bea2632067d4a62066bef1aa14926
Paul Iov <pauliovster@gmail.com>
2024-10-02 21:26:40 +0200

    chore: add Spartan-NG UI components

--- frontend/projects/recipe-box/src/app/app.component.ts
+++ frontend/projects/recipe-box/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { lucideMail } from '@ng-icons/lucide';
+import { provideIcons } from '@ng-icons/core';
+
+import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HlmButtonDirective, HlmIconComponent],
+  providers: [provideIcons({ lucideMail })],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,13 +1,13 @@
-// Type import not required in .ts
-// /** @type {import('tailwindcss').Config} */
-//
+import type { Config } from "tailwindcss";
+
 export default {
+  presets: [require('@spartan-ng/ui-core/hlm-tailwind-preset')],
   content: [
     './projects/**/*.{html,ts}',
+    './libs/spartan/**/*.{html,ts}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+} satisfies Config;
